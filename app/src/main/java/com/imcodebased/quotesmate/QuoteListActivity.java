2a8dcf43fd15ee01d7d2e8018cfbdb0eded4a314
codebased <codebased@hotmail.com>
2016-07-13 17:54:53 +1000

    Implementing a custom model quote with simple list item 1 and using interface for separation of concerns

--- app/src/main/java/com/imcodebased/quotesmate/QuoteListActivity.java
+++ app/src/main/java/com/imcodebased/quotesmate/QuoteListActivity.java
@@ -8,49 +8,53 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.List;
+
+import helpers.IntentUtil;
+import model.Quote;
+import model.Services.IQuotesDataLoader;
+import model.Services.StaticQuotesDataLoader;
 
 public class QuoteListActivity extends AppCompatActivity {
 
     private ListView listView;
 
-    private ArrayList<String> collection;
+    private List<Quote> quotes;
 
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<Quote> adapter;
+
+    private IQuotesDataLoader quotesDataLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_list);
-        populateValue();
+        quotesDataLoader = new StaticQuotesDataLoader();
         initUI();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        initData();
     }
 
-    private void populateValue() {
-        collection = new ArrayList<>();
-        collection.add("Item1");
-        collection.add("Item2");
-        collection.add("Item3");
-        collection.add("Item4");
-        collection.add("Item5");
-    }
+    private void initData() {
 
-    private void initUI() {
-        listView = (ListView) findViewById(R.id.listView);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, collection);
+        quotes = quotesDataLoader.getAll();
 
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, quotes);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(QuoteListActivity.this, collection.get(position), Toast.LENGTH_LONG).show();
+                Toast.makeText(QuoteListActivity.this, quotes.get(position).toString(), Toast.LENGTH_LONG).show();
+                startActivity(IntentUtil.createShareIntent(quotes.get(position).toString()));
             }
         });
+    }
 
+    private void initUI() {
+        listView = (ListView) findViewById(R.id.listView);
     }
 }
---
+++ app/src/main/java/model/Services/IQuotesDataLoader.java
@@ -0,0 +1,9 @@
+package model.Services;
+
+import java.util.List;
+
+import model.Quote;
+
+public interface IQuotesDataLoader {
+     List<Quote> getAll();
+}
