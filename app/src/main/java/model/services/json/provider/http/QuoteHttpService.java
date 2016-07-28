6687899d370a8d6be4697a6a8aab51cb851e947c
codebased <codebased@hotmail.com>
2016-07-28 21:46:47 +1000

    Adding custom header insertion through retro2

--- app/src/main/java/model/services/json/provider/http/QuoteHttpService.java
+++ app/src/main/java/model/services/json/provider/http/QuoteHttpService.java
@@ -4,18 +4,24 @@ package model.services.json.provider.http;
 import model.Quotes;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 // retrofit2 uses  OkHttp as the networking layer and is built on top of it.
 public interface QuoteHttpService {
 
-
+    @Headers("Cache-Control: max-age=640000")
     @GET("/quotes/randomize")
     Call<Quotes> randomize(@Query("count") int count);
 
+    @Headers({
+            "Accept: application/vnd.yourapi.v1.full+json",
+            "User-Agent: Your-App-Name"
+    })
     @GET("/quotes/today")
-    Call<Quotes> today();
+    Call<Quotes> today(@Header("x-custom-message") String message);
 
     @GET("/quotes/genres/{genre}")
     Call<Quotes> genres(@Path("genre") String genre);
--- app/src/main/java/model/services/json/provider/http/ServiceGenerator.java
+++ app/src/main/java/model/services/json/provider/http/ServiceGenerator.java
@@ -1,6 +1,13 @@
 package model.services.json.provider.http;
 
+import android.util.Base64;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,6 +25,33 @@ public class ServiceGenerator {
 
     public static <S> S createService(Class<S> serviceClass) {
         Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+
+    public static <S> S createService(Class<S> serviceClass, String username, String password) {
+        if (username != null && password != null) {
+            String credentials = username + ":" + password;
+            final String basic =
+                    "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .header("Authorization", basic)
+                            .header("Accept", "application/json")
+                            .method(original.method(), original.body());
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            });
+        }
+
+        OkHttpClient client = httpClient.build();
+        Retrofit retrofit = builder.client(client).build();
         return retrofit.create(serviceClass);
     }
 }
